bf6a5b5a25c11243e4ed2476e2473d788741d19e
Maugli <maugli10121977@gmail.com>
2021-03-15 08:50:29 +0300

    Парсинг словаря 'en_to_ru' и повторение отмеченных слов.

---
+++ repeat_en_to_ru.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import os
+import sqlite3
+import random
+from time import sleep
+from datetime import datetime
+
+period = int(input("Время паузы:   "))
+print("\n")
+
+def date_time():
+    date = datetime.now()
+    time = date.strftime("%d/%m/%Y %H:%M:%S")
+    return time
+
+if 'en_to_ru.db' in os.listdir():
+    os.system('rm en_to_ru.db')
+
+os.system('wget -c http://www.ieroglif1977.ru/cgi-bin/my_dictionary/en_to_ru.db')
+
+db = sqlite3.connect('en_to_ru.db')
+curs = db.cursor()
+my_dict = sorted(curs.execute('select * from dictionary;').fetchall())
+
+#print(f'{my_dict}')
+repeated_words = []
+for i in range(len(my_dict)):
+    if my_dict[i][2] == 1:
+        repeated_words.append(my_dict[i])
+
+random.shuffle(repeated_words)
+
+#print(f'{repeated_words}')
+print(f'Всего в словаре {len(my_dict)} слов.')
+print(f'Повторяемых слов:   {len(repeated_words)}')
+print('\n')
+
+def repeat():
+    print(f'{date_time()}')
+    print('\n')
+    for i in range(len(repeated_words)):
+        sleep(period)
+        print(f'{i+1}. {repeated_words[i][0]} ==> {repeated_words[i][1]}')
+    print('\n')
+    print(f'{date_time()}\nThe dictionary ended.')
+
+try:
+    repeat()
+except KeyboardInterrupt:
+    exit()
